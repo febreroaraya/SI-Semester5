113b4ee231d1ff797f8b44b0932d5b0219692284
ajipratama11 <47959982+ajipratama11@users.noreply.github.com>
2020-11-06 01:15:27 +0700

    finish config CI

---
+++ application/views/template/navbar.php
@@ -0,0 +1,262 @@
+<body>
+    
+    <div class="cart" data-toggle="inactive">
+      <div class="label">
+        <i class="ion-bag"></i> 7
+      </div>
+
+      <div class="overlay"></div>
+
+      <div class="window">
+        <div class="title">
+          <button type="button" class="close"><i class="ion-android-close"></i></button>
+          <h4>Shopping cart</h4>
+        </div>
+
+        <div class="content">
+
+          <div class="media">
+            <div class="media-left">
+              <a href="#">
+                <img class="media-object" src="<?= base_url(); ?>assets/frontend/img/products/chrome-book-11.jpg" alt="HP Chromebook 11"/>
+              </a>
+            </div>
+            <div class="media-body">
+              <h4 class="media-heading">HP Chromebook 11</h4>
+              <label>Laptops</label>
+              <p class="price">$199.99</p>
+            </div>
+            <div class="controls">
+              <div class="input-group">
+                <span class="input-group-btn">
+                  <button class="btn btn-default btn-sm" type="button" data-action="minus"><i class="ion-minus-round"></i></button>
+                </span>
+                <input type="text" class="form-control input-sm" placeholder="Qty" value="1" readonly="">
+                <span class="input-group-btn">
+                  <button class="btn btn-default btn-sm" type="button" data-action="plus"><i class="ion-plus-round"></i></button>
+                </span>
+              </div><!-- /input-group -->
+
+              <a href="#remove"> <i class="ion-trash-b"></i> Remove </a>
+            </div>
+          </div>
+
+          <div class="media">
+            <div class="media-left">
+              <a href="#">
+                <img class="media-object" src="<?= base_url(); ?>assets/frontend/img/products/chrome-book-14.jpg" alt="HP Chromebook 11"/>
+              </a>
+            </div>
+            <div class="media-body">
+              <h4 class="h4 media-heading">HP Chromebook 14</h4>
+              <label>Laptops</label>
+              <p class="price">$209.99</p>
+            </div>
+            <div class="controls">
+              <div class="input-group">
+                <span class="input-group-btn">
+                  <button class="btn btn-default btn-sm" type="button" data-action="minus"><i class="ion-minus-round"></i></button>
+                </span>
+                <input type="text" class="form-control input-sm" placeholder="Qty" value="1" readonly="">
+                <span class="input-group-btn">
+                  <button class="btn btn-default btn-sm" type="button" data-action="plus"><i class="ion-plus-round"></i></button>
+                </span>
+              </div><!-- /input-group -->
+
+              <a href="#remove"> <i class="ion-trash-b"></i> Remove </a>
+            </div>
+          </div>
+
+          <div class="media">
+            <div class="media-left">
+              <a href="#">
+                <img class="media-object" src="<?= base_url(); ?>assets/frontend/img/products/ipad-air.jpg" alt="iPad Air"/>
+              </a>
+            </div>
+            <div class="media-body">
+              <h4 class="h4 media-heading">iPad Air</h4>
+              <label>Tablets</label>
+              <p class="price">$449.99</p>
+            </div>
+            <div class="controls">
+              <div class="input-group">
+                <span class="input-group-btn">
+                  <button class="btn btn-default btn-sm" type="button" data-action="minus"><i class="ion-minus-round"></i></button>
+                </span>
+                <input type="text" class="form-control input-sm" placeholder="Qty" value="1" readonly="">
+                <span class="input-group-btn">
+                  <button class="btn btn-default btn-sm" type="button" data-action="plus"><i class="ion-plus-round"></i></button>
+                </span>
+              </div><!-- /input-group -->
+
+              <a href="#remove"> <i class="ion-trash-b"></i> Remove </a>
+            </div>
+          </div>
+
+          <div class="media">
+            <div class="media-left">
+              <a href="#">
+                <img class="media-object" src="<?= base_url(); ?>assets/frontend/img/products/mi-pad-2.jpg" alt="HP Chromebook 11"/>
+              </a>
+            </div>
+            <div class="media-body">
+              <h4 class="h4 media-heading">Mi Pad 2</h4>
+              <label>Tablets</label>
+              <p class="price">$199.99</p>
+            </div>
+            <div class="controls">
+              <div class="input-group">
+                <span class="input-group-btn">
+                  <button class="btn btn-default btn-sm" type="button" data-action="minus"><i class="ion-minus-round"></i></button>
+                </span>
+                <input type="text" class="form-control input-sm" placeholder="Qty" value="1" readonly="">
+                <span class="input-group-btn">
+                  <button class="btn btn-default btn-sm" type="button" data-action="plus"><i class="ion-plus-round"></i></button>
+                </span>
+              </div><!-- /input-group -->
+
+              <a href="#remove"> <i class="ion-trash-b"></i> Remove </a>
+            </div>
+          </div>
+
+          <div class="media">
+            <div class="media-left">
+              <a href="#">
+                <img class="media-object" src="<?= base_url(); ?>assets/frontend/img/products/lenovo-yoga.jpg" alt="Lenovo Yoga"/>
+              </a>
+            </div>
+            <div class="media-body">
+              <h4 class="h4 media-heading">Lenovo Yoga</h4>
+              <label>Hybrid</label>
+              <p class="price">$199.99</p>
+            </div>
+            <div class="controls">
+              <div class="input-group">
+                <span class="input-group-btn">
+                  <button class="btn btn-default btn-sm" type="button" data-action="minus"><i class="ion-minus-round"></i></button>
+                </span>
+                <input type="text" class="form-control input-sm" placeholder="Qty" value="1" readonly="">
+                <span class="input-group-btn">
+                  <button class="btn btn-default btn-sm" type="button" data-action="plus"><i class="ion-plus-round"></i></button>
+                </span>
+              </div><!-- /input-group -->
+
+              <a href="#remove"> <i class="ion-trash-b"></i> Remove </a>
+            </div>
+          </div>
+
+          <div class="media">
+            <div class="media-left">
+              <a href="#">
+                <img class="media-object" src="<?= base_url(); ?>assets/frontend/img/products/surface-pro.jpg" alt="HP Chromebook 11"/>
+              </a>
+            </div>
+            <div class="media-body">
+              <h4 class="h4 media-heading">Surface Pro</h4>
+              <label>Hybrid</label>
+              <p class="price">$199.99</p>
+            </div>
+            <div class="controls">
+              <div class="input-group">
+                <span class="input-group-btn">
+                  <button class="btn btn-default btn-sm" type="button" data-action="minus"><i class="ion-minus-round"></i></button>
+                </span>
+                <input type="text" class="form-control input-sm" placeholder="Qty" value="1" readonly="">
+                <span class="input-group-btn">
+                  <button class="btn btn-default btn-sm" type="button" data-action="plus"><i class="ion-plus-round"></i></button>
+                </span>
+              </div><!-- /input-group -->
+
+              <a href="#remove"> <i class="ion-trash-b"></i> Remove </a>
+            </div>
+          </div>
+
+          <div class="media">
+            <div class="media-left">
+              <a href="#">
+                <img class="media-object" src="<?= base_url(); ?>assets/frontend/img/products/asus-transformer.jpg" alt="HP Chromebook 11"/>
+              </a>
+            </div>
+            <div class="media-body">
+              <h4 class="h4 media-heading">ASUS Transformer</h4>
+              <label>Hybrid</label>
+              <p class="price">$199.99</p>
+            </div>
+            <div class="controls">
+              <div class="input-group">
+                <span class="input-group-btn">
+                  <button class="btn btn-default btn-sm" type="button" data-action="minus"><i class="ion-minus-round"></i></button>
+                </span>
+                <input type="text" class="form-control input-sm" placeholder="Qty" value="1" readonly="">
+                <span class="input-group-btn">
+                  <button class="btn btn-default btn-sm" type="button" data-action="plus"><i class="ion-plus-round"></i></button>
+                </span>
+              </div><!-- /input-group -->
+
+              <a href="#remove"> <i class="ion-trash-b"></i> Remove </a>
+            </div>
+          </div>
+
+        </div>
+
+        <div class="checkout container-fluid">
+          <div class="row">
+            <div class="col-xs-12 col-sm-12 align-right">
+              <a class="btn btn-primary" href="checkout/"> Checkout order </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+    <div class="toplinks">
+      <a href="#signin" data-toggle="modal" data-target="#Modal-Registration"> <i class="ion-person"></i> Daftar</a>
+      <a href="#signin" data-toggle="modal" data-target="#Modal-SignIn"> <i class="ion-unlocked"></i> Masuk</a>
+      <a href="./favorites/"> <i class="ion-ios-heart"></i> Favorit </a>
+      <a href="tel:+80005554465" class="hidden-xs"> <i class="ion-android-call"></i> 8000 555-44-65 </a>
+    </div>
+
+    
+    <nav class="navbar navbar-default">
+        <div class="container">
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+              <span class="sr-only">Toggle navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="./"> RDPVision </a>
+            <a class="navbar-brand pull-right hidden-sm hidden-md hidden-lg" href="#open-cart"> <i class="ion-bag"></i> 7 </a>
+          </div>
+
+          <div id="navbar" class="navbar-collapse collapse">
+            <ul class="nav navbar-nav">
+              <li class="active"><a href="./">Beranda</a></li>
+              <li><a href="<?= base_url('Store') ?>">Store</a></li>
+              <li><a href="./contacts/">Kontak</a></li>
+              <li class="dropdown">
+                <a href="./store/" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+                  Lainnya <i class="ion-android-arrow-dropdown"></i>
+                </a>
+                <ul class="dropdown-menu">
+                  <li><a href="./store/product.html">Produk</a></li>
+                  <li><a href="./about/">Tentang Kami</a></li>
+                  <li><a href="./checkout/">Pembayaran</a></li>
+                  <li><a href="./gallery/">Galeri</a></li>
+                </ul>
+              </li>
+            </ul>
+          </div><!--/.nav-collapse -->
+
+
+          <div class="search hidden-xs" data-style="hidden">
+            <div class="input">
+              <button type="button"><i class="ion-ios-search"></i></button>
+
+              <input type="text" name="search" value="" placeholder="Cari" />
+            </div>
+          </div>
+        </div><!--/.container-fluid -->
+    </nav>
