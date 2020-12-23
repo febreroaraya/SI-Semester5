95a36ad5d857b8ca182bddcdca599cd81f9e28f8
febreroaraya <55268432+febreroaraya@users.noreply.github.com>
2020-12-23 12:55:22 +0700

    Update index.php

--- index.php
+++ index.php
@@ -70,7 +70,7 @@
 
         <div class="items">
           <div class="item" data-marker="1">
-            <img src="../SI-Semester5/assets/img/carousel/4.jpg" alt="Background" class="background"/>
+            <img src="../SI-Semester5/assets/img/carousel/3.jpg" alt="Background" class="background"/>
 
             <div class="content">
               <div class="outside-content">
@@ -91,7 +91,7 @@
             </div>
           </div>
           <div class="item active" data-marker="2">
-            <img src="../SI-Semester5/assets/img/carousel/3.jpg" alt="Background" class="background"/>
+            <img src="../SI-Semester5/assets/img/carousel/4.jpg" alt="Background" class="background"/>
 
             <div class="content">
               <div class="outside-content">
@@ -188,40 +188,7 @@
     <br><br>
 
     <footer>
-      <div class="about">
-        <div class="container">
-          <div class="row">
-            <hr class="offset-md">
-
-            <div class="col-xs-6 col-sm-3">
-              <div class="item">
-                <i class="ion-ios-telephone-outline"></i>
-                <h1>24/7 free <br> <span>support</span></h1>
-              </div>
-            </div>
-            <div class="col-xs-6 col-sm-3">
-              <div class="item">
-                <i class="ion-ios-star-outline"></i>
-                <h1>Low price <br> <span>guarantee</span></h1>
-              </div>
-            </div>
-            <div class="col-xs-6 col-sm-3">
-              <div class="item">
-                <i class="ion-ios-gear-outline"></i>
-                <h1> Manufacturer’s <br> <span>warranty</span></h1>
-              </div>
-            </div>
-            <div class="col-xs-6 col-sm-3">
-              <div class="item">
-                <i class="ion-ios-loop"></i>
-                <h1> Full refund <br> <span>guarantee</span> </h1>
-              </div>
-            </div>
-
-            <hr class="offset-md">
-          </div>
-        </div>
-      </div>
+      
 
       <div class="subscribe">
         <div class="container align-center">
@@ -234,10 +201,7 @@
             <div class="social">
               <a href="#"><i class="ion-social-facebook"></i></a>
               <a href="#"><i class="ion-social-twitter"></i></a>
-              <a href="#"><i class="ion-social-googleplus-outline"></i></a>
               <a href="#"><i class="ion-social-instagram-outline"></i></a>
-              <a href="#"><i class="ion-social-linkedin-outline"></i></a>
-              <a href="#"><i class="ion-social-youtube-outline"></i></a>
             </div>
 
 
@@ -256,7 +220,6 @@
 
             <div class="list-group">
               <a href="#" class="list-group-item">About</a>
-              <a href="#" class="list-group-item">How to order</a>
             </div>
           </div>
           <div class="col-sm-3 col-md-2">
@@ -277,38 +240,8 @@
               <a href="#" class="list-group-item">Contacts</a>
             </div>
           </div>
-          <div class="col-sm-3 col-md-2">
-            <h1 class="h4">Location</h1>
-
-            <div class="dropdown">
-              <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                Language
-                <span class="caret"></span>
-              </button>
-              <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
-                <li><a href="#English"> <img src="assets/img/flags/gb.png" alt="Eng"/> English</a></li>
-                <li><a href="#Spanish"> <img src="assets/img/flags/es.png" alt="Spa"/> Spanish</a></li>
-                <li><a href="#Deutch"> <img src="assets/img/flags/de.png" alt="De"/> Deutch</a></li>
-                <li><a href="#French"> <img src="assets/img/flags/fr.png" alt="Fr"/> French</a></li>
-              </ul>
-            </div>
-            <hr class="offset-xs">
-
-            <div class="dropdown">
-              <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                Currency
-                <span class="caret"></span>
-              </button>
-              <ul class="dropdown-menu" aria-labelledby="dropdownMenu2">
-                <li><a href="#Euro"><i class="ion-social-euro"></i> Euro</a></li>
-                <li><a href="#Dollar"><i class="ion-social-usd"></i> Dollar</a></li>
-                <li><a href="#Yen"><i class="ion-social-yen"></i> Yen</a></li>
-                <li><a href="#Bitcoin"><i class="ion-social-bitcoin"></i> Bitcoin</a></li>
-              </ul>
-            </div>
-
-          </div>
-          <div class="col-sm-3 col-md-3 col-md-offset-1 align-right hidden-sm hidden-xs">
+         
+          <div class="col-sm-3 col-md-5 col-md-offset-1 align-right hidden-sm hidden-xs">
             <h1 class="h4">RDPVision</h1>
 
               <address>
