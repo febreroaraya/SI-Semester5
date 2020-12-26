81217c8c59920547871289351840097df288425e
Ahmad Dandi Irawan <ahmaddandiirawan@gmail.com>
2020-12-26 13:37:26 +0700

    tanggal

--- application/models/Model_invoice.php
+++ application/models/Model_invoice.php
@@ -3,7 +3,7 @@
 class Model_invoice extends CI_Model{
     public function index ()
     {
-       
+        date_default_timezone_set('Asia/Jakarta');
         $nama     = $this->input->post('nama');
         $alamat   = $this->input->post('alamat');
         
