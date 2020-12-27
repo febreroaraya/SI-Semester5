1a42c3c33d424e0d933e4ec92a8b03e0078fba61
febreroaraya <55268432+febreroaraya@users.noreply.github.com>
2020-12-27 12:25:06 +0700

    29 done

--- application/controllers/admin/Invoice.php
+++ application/controllers/admin/Invoice.php
@@ -12,7 +12,7 @@ class Invoice extends CI_Controller{
     public function detail($id_invoice)
     {
         $data['invoice'] = $this->model_invoice->ambil_id_invoice($id_invoice);
-        $data['pesanan'] = $this->model_invoice->ambil_id_invoice($id_invoice);
+        $data['pesanan'] = $this->model_invoice->ambil_id_pesanan($id_invoice);
         $this->load->view('templates_admin/header');
         $this->load->view('templates_admin/sidebar');
         $this->load->view('admin/detail_invoice', $data);
